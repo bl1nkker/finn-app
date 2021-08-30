176af068209e26b7672350a7a75012de4041fc21
Auezkhan Daniyar <nurovich14@gmail.com>
2021-08-30 16:05:20 +0600

    added https server

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://137.184.15.87/api' })
+const API = axios.create({ baseURL: 'https://finapp.rcbxd.dev/' })
 // const API = axios.create({ baseURL: 'http://127.0.0.1:8000/api' })
 
 // This adds a headers to each request. It happens before all functions below
